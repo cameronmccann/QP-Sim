4b39f4ddb7d12ecba03bc418f7c438003cab0fa5
Cameron McCann <cameronmccann@utexas.edu>
2024-03-21 10:48:30 -0500

    Add code to display relative bias table for Simulation 1

--- Code/S1_Obtain-Results.R
+++ Code/S1_Obtain-Results.R
@@ -112,7 +112,7 @@ perf_measure_DF <- sim1_data %>%
   group_by(ICC, clust_size, 
            conditionNum, analysisCond) %>% 
   summarize(TNDE_relBias = (mean(TNDE_est) / TNDE) - 1, 
-            NIE_relBias = (mean(PNIE_est) / PNIE) - 1, 
+            PNIE_relBias = (mean(PNIE_est) / PNIE) - 1, 
             
             # TNDE_MSE = (mean(TNDE_est) - TNDE)^2 + (sd(TNDE_est)^2), 
             # PNIE_MSE = (mean(PNIE_est) - PNIE)^2 + (sd(PNIE_est)^2), 
@@ -130,4 +130,75 @@ write_rds(perf_measure_DF,
 
 write_rds(sim1_data, 
           file = paste0("Output/S1_Results/Data/", "S1_Simulation-Data.rds"))
+
+
+
+# TNDE Relative Bias Table ------------------------------------------------
+
+# Pivot wide 
+relBias_Tbl <- pivot_wider(
+  perf_measure_DF[, c("ICC",
+                      "clust_size",
+                      "analysisCond",
+                      "TNDE_relBias",
+                      "PNIE_relBias")],
+  names_from = c(ICC, clust_size),
+  names_sep = "_",
+  values_from = c("TNDE_relBias", "PNIE_relBias")
+)
+
+# drop common peice of column names 
+colnames(relBias_Tbl) <-
+  stringr::str_remove(string = colnames(relBias_Tbl),
+                      pattern = "_relBias")
+
+# Relative Bias TNDE Table   
+relBias_TNDE_Table <- relBias_Tbl %>% 
+  select(analysisCond, starts_with("TNDE_")) %>% 
+  separate(col = c("analysisCond"), into = c("PS Model", "Mediator/Outcome Model"), sep = "_") %>% 
+  arrange(`Mediator/Outcome Model`)
+
+# Display values (Copy-paste values into generate table website to obtain latex code)
+as_hux(relBias_TNDE_Table) %>% 
+  set_number_format(row = 1:nrow(relBias_TNDE_Table) + 1, col = -c(1:2), value = 3)
+
+
+
+# PNIE Relative Bias Table ------------------------------------------------
+
+# Pivot wide
+relBias_Tbl <- pivot_wider(
+  perf_measure_DF[, c("ICC",
+                      "clust_size",
+                      "analysisCond",
+                      "TNDE_relBias",
+                      "PNIE_relBias")],
+  names_from = c(ICC, clust_size),
+  names_sep = "_",
+  values_from = c("TNDE_relBias", "PNIE_relBias")
+)
+
+# drop common peice of column names
+colnames(relBias_Tbl) <-
+  stringr::str_remove(string = colnames(relBias_Tbl),
+                      pattern = "_relBias")
+
+# Relative Bias PNIE Table   
+relBias_PNIE_Table <- relBias_Tbl %>%
+  select(analysisCond, starts_with("PNIE_")) %>%
+  separate(
+    col = c("analysisCond"),
+    into = c("PS Model", "Mediator/Outcome Model"),
+    sep = "_"
+  ) %>%
+  arrange(`Mediator/Outcome Model`)
+
+# Display values (Copy-paste values into generate table website to obtain latex code)
+as_hux(relBias_PNIE_Table) %>%
+  set_number_format(
+    row = 1:nrow(relBias_PNIE_Table) + 1,
+    col = -c(1:2),
+    value = 3
+  )
+
 
